a390d3060350ba20a224649ff83b688dda5d8e6f
guzin12 <159173948+guzin12@users.noreply.github.com>
2024-08-13 13:46:30 -0300

    Add files via upload

---
+++ dump.sqlite (3).sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE alunos(
+  id int(3) unique,
+  nome varchar(50)Not NUll,
+  idade varchar(3),
+  turma varchar(4)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
